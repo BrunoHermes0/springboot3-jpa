71caa2aadc86aa18bd8e0eadffc4bdcc2db2b58c
BrunoHermes0 <bruno.hermes9@gmail.com>
2024-05-12 14:53:13 -0300

    User entity and Resource

---
+++ src/main/java/com/bruno/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.bruno.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bruno.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria" , "mariaa@gmail.com", "9919929", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+}
